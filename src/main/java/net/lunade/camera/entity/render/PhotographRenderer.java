dcac60756ff50ba0da5905d573180a7a97fe0ebc
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-09-25 01:54:46 -0400

    fix photograph rendering

--- src/main/java/net/lunade/camera/entity/render/PhotographRenderer.java
+++ src/main/java/net/lunade/camera/entity/render/PhotographRenderer.java
@@ -21,6 +21,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @Environment(EnvType.CLIENT)
 public class PhotographRenderer extends EntityRenderer<Photograph> {
@@ -30,7 +31,7 @@ public class PhotographRenderer extends EntityRenderer<Photograph> {
     }
 
     @Override
-    public void render(Photograph photograph, float f, float g, @NotNull PoseStack matrices, @NotNull MultiBufferSource photoConsumers, int i) {
+    public void render(Photograph photograph, float f, float g, @NotNull PoseStack matrices, @NotNull MultiBufferSource vertexConsumers, int i) {
         String photographName = photograph.getPhotographName();
         ResourceLocation photoLocation = this.getPhotographLocation(photograph);
         if (photograph.serverTexture == null && !photographName.isEmpty()) {
@@ -44,18 +45,24 @@ public class PhotographRenderer extends EntityRenderer<Photograph> {
         }
         matrices.pushPose();
         matrices.mulPose(Axis.YP.rotationDegrees(180F - f));
-        VertexConsumer photoConsumer = photoConsumers.getBuffer(RenderType.entitySolid(photoLocation));
-        final var backSprite = Minecraft.getInstance().getPaintingTextures().getBackSprite();
-        VertexConsumer backConsumer = photoConsumers.getBuffer(RenderType.entitySolid(backSprite.atlasLocation()));
         this.renderPhotograph(
                 matrices,
-                photoConsumer,
                 photograph,
-                backConsumer,
-                backSprite
+                vertexConsumers.getBuffer(RenderType.entitySolid(photoLocation)),
+                null,
+                false
+        );
+
+        final var backSprite = Minecraft.getInstance().getPaintingTextures().getBackSprite();
+        this.renderPhotograph(
+                matrices,
+                photograph,
+                vertexConsumers.getBuffer(RenderType.entitySolid(backSprite.atlasLocation())),
+                backSprite,
+                true
         );
         matrices.popPose();
-        super.render(photograph, f, g, matrices, photoConsumers, i);
+        super.render(photograph, f, g, matrices, vertexConsumers, i);
     }
 
     @Override
@@ -68,52 +75,42 @@ public class PhotographRenderer extends EntityRenderer<Photograph> {
     }
 
     private void renderPhotograph(
-            @NotNull PoseStack matrices, VertexConsumer photoConsumer, Photograph entity, VertexConsumer backConsumer, TextureAtlasSprite backSprite
+            @NotNull PoseStack matrices, @NotNull Photograph entity, VertexConsumer vertexConsumer, @Nullable TextureAtlasSprite backSprite, boolean renderingSide
     ) {
+        if (renderingSide && backSprite == null) throw new IllegalArgumentException("backSprite cannot be null while rendering the side of a photograph!");
         PoseStack.Pose pose = matrices.last();
-        float f = (float) (-2D) / 2.0F;
-        float g = (float) (-2D) / 2.0F;
-        float h = 0.03125F;
-        float i = backSprite.getU0();
-        float j = backSprite.getU1();
-        float k = backSprite.getV0();
-        float l = backSprite.getV1();
-        float m = backSprite.getU0();
-        float n = backSprite.getU1();
-        float o = backSprite.getV0();
-        float p = backSprite.getV(0.0625F);
-        float q = backSprite.getU0();
-        float r = backSprite.getU(0.0625F);
-        float s = backSprite.getV0();
-        float t = backSprite.getV1();
-        double d = 1.0 / (double) (int) 2.0;
-        double e = 1.0 / (double) (int) 2.0;
+        float photographSize = entity.getSize();
+        float f = (-photographSize) / 2F;
+        float i = !renderingSide ? 0F : backSprite.getU0();
+        float j = !renderingSide ? 0F : backSprite.getU1();
+        float k = !renderingSide ? 0F : backSprite.getV0();
+        float l = !renderingSide ? 0F : backSprite.getV1();
+        float m = !renderingSide ? 0F : backSprite.getU0();
+        float n = !renderingSide ? 0F : backSprite.getU1();
+        float o = !renderingSide ? 0F : backSprite.getV0();
+        float p = !renderingSide ? 0F : backSprite.getV(0.0625F);
+        float q = !renderingSide ? 0F : backSprite.getU0();
+        float r = !renderingSide ? 0F : backSprite.getU(0.0625F);
+        float s = !renderingSide ? 0F : backSprite.getV0();
+        float t = !renderingSide ? 0F : backSprite.getV1();
+        double d = 1D / photographSize;
+        double e = 1D / photographSize;
 
-        for (int u = 0; u < (int) 2.0; u++) {
-            for (int v = 0; v < (int) 2.0; v++) {
-                float w = f + (float) (u + 1);
+        for (int u = 0; u < 2; u++) {
+            for (int v = 0; v < 2; v++) {
+                float w = f + (u + 1F);
                 float x = f + (float) u;
-                float y = g + (float) (v + 1);
-                float z = g + (float) v;
+                float y = f + (v + 1F);
+                float z = f + (float) v;
                 int aa = entity.getBlockX();
-                int ab = Mth.floor(entity.getY() + (double) ((y + z) / 2.0F));
+                int ab = Mth.floor(entity.getY() + (double) ((y + z) / 2F));
                 int ac = entity.getBlockZ();
                 Direction direction = entity.getDirection();
-                if (direction == Direction.NORTH) {
-                    aa = Mth.floor(entity.getX() + (double) ((w + x) / 2.0F));
-                }
-
-                if (direction == Direction.WEST) {
-                    ac = Mth.floor(entity.getZ() - (double) ((w + x) / 2.0F));
-                }
-
-                if (direction == Direction.SOUTH) {
-                    aa = Mth.floor(entity.getX() - (double) ((w + x) / 2.0F));
-                }
-
-                if (direction == Direction.EAST) {
-                    ac = Mth.floor(entity.getZ() + (double) ((w + x) / 2.0F));
-                }
+                
+                if (direction == Direction.NORTH) aa = Mth.floor(entity.getX() + (double) ((w + x) / 2F));
+                if (direction == Direction.WEST) ac = Mth.floor(entity.getZ() - (double) ((w + x) / 2F));
+                if (direction == Direction.SOUTH) aa = Mth.floor(entity.getX() - (double) ((w + x) / 2F));
+                if (direction == Direction.EAST) ac = Mth.floor(entity.getZ() + (double) ((w + x) / 2F));
 
                 int ad = LevelRenderer.getLightColor(entity.level(), new BlockPos(aa, ab, ac));
 
@@ -122,42 +119,42 @@ public class PhotographRenderer extends EntityRenderer<Photograph> {
                 float af = (float)(d * (size - (u + 1F)));
                 float ag = (float)(e * (size - v));
                 float ah = (float)(e * (size - (v + 1F)));
-                matrices.pushPose();
-                this.vertex(pose, photoConsumer, w, z, af, ag, -0.03125F, 0, 0, -1, ad);
-                this.vertex(pose, photoConsumer, x, z, ae, ag, -0.03125F, 0, 0, -1, ad);
-                this.vertex(pose, photoConsumer, x, y, ae, ah, -0.03125F, 0, 0, -1, ad);
-                this.vertex(pose, photoConsumer, w, y, af, ah, -0.03125F, 0, 0, -1, ad);
-                matrices.popPose();
-                matrices.pushPose();
-                this.vertex(pose, backConsumer, w, y, j, k, 0.03125F, 0, 0, 1, ad);
-                this.vertex(pose, backConsumer, x, y, i, k, 0.03125F, 0, 0, 1, ad);
-                this.vertex(pose, backConsumer, x, z, i, l, 0.03125F, 0, 0, 1, ad);
-                this.vertex(pose, backConsumer, w, z, j, l, 0.03125F, 0, 0, 1, ad);
-                this.vertex(pose, backConsumer, w, y, m, o, -0.03125F, 0, 1, 0, ad);
-                this.vertex(pose, backConsumer, x, y, n, o, -0.03125F, 0, 1, 0, ad);
-                this.vertex(pose, backConsumer, x, y, n, p, 0.03125F, 0, 1, 0, ad);
-                this.vertex(pose, backConsumer, w, y, m, p, 0.03125F, 0, 1, 0, ad);
-                this.vertex(pose, backConsumer, w, z, m, o, 0.03125F, 0, -1, 0, ad);
-                this.vertex(pose, backConsumer, x, z, n, o, 0.03125F, 0, -1, 0, ad);
-                this.vertex(pose, backConsumer, x, z, n, p, -0.03125F, 0, -1, 0, ad);
-                this.vertex(pose, backConsumer, w, z, m, p, -0.03125F, 0, -1, 0, ad);
-                this.vertex(pose, backConsumer, w, y, r, s, 0.03125F, -1, 0, 0, ad);
-                this.vertex(pose, backConsumer, w, z, r, t, 0.03125F, -1, 0, 0, ad);
-                this.vertex(pose, backConsumer, w, z, q, t, -0.03125F, -1, 0, 0, ad);
-                this.vertex(pose, backConsumer, w, y, q, s, -0.03125F, -1, 0, 0, ad);
-                this.vertex(pose, backConsumer, x, y, r, s, -0.03125F, 1, 0, 0, ad);
-                this.vertex(pose, backConsumer, x, z, r, t, -0.03125F, 1, 0, 0, ad);
-                this.vertex(pose, backConsumer, x, z, q, t, 0.03125F, 1, 0, 0, ad);
-                this.vertex(pose, backConsumer, x, y, q, s, 0.03125F, 1, 0, 0, ad);
-                matrices.popPose();
+
+                if (!renderingSide) {
+                    this.vertex(pose, vertexConsumer, w, z, af, ag, -0.03125F, 0, 0, -1, ad);
+                    this.vertex(pose, vertexConsumer, x, z, ae, ag, -0.03125F, 0, 0, -1, ad);
+                    this.vertex(pose, vertexConsumer, x, y, ae, ah, -0.03125F, 0, 0, -1, ad);
+                    this.vertex(pose, vertexConsumer, w, y, af, ah, -0.03125F, 0, 0, -1, ad);
+                } else {
+                    this.vertex(pose, vertexConsumer, w, y, j, k, 0.03125F, 0, 0, 1, ad);
+                    this.vertex(pose, vertexConsumer, x, y, i, k, 0.03125F, 0, 0, 1, ad);
+                    this.vertex(pose, vertexConsumer, x, z, i, l, 0.03125F, 0, 0, 1, ad);
+                    this.vertex(pose, vertexConsumer, w, z, j, l, 0.03125F, 0, 0, 1, ad);
+                    this.vertex(pose, vertexConsumer, w, y, m, o, -0.03125F, 0, 1, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, y, n, o, -0.03125F, 0, 1, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, y, n, p, 0.03125F, 0, 1, 0, ad);
+                    this.vertex(pose, vertexConsumer, w, y, m, p, 0.03125F, 0, 1, 0, ad);
+                    this.vertex(pose, vertexConsumer, w, z, m, o, 0.03125F, 0, -1, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, z, n, o, 0.03125F, 0, -1, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, z, n, p, -0.03125F, 0, -1, 0, ad);
+                    this.vertex(pose, vertexConsumer, w, z, m, p, -0.03125F, 0, -1, 0, ad);
+                    this.vertex(pose, vertexConsumer, w, y, r, s, 0.03125F, -1, 0, 0, ad);
+                    this.vertex(pose, vertexConsumer, w, z, r, t, 0.03125F, -1, 0, 0, ad);
+                    this.vertex(pose, vertexConsumer, w, z, q, t, -0.03125F, -1, 0, 0, ad);
+                    this.vertex(pose, vertexConsumer, w, y, q, s, -0.03125F, -1, 0, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, y, r, s, -0.03125F, 1, 0, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, z, r, t, -0.03125F, 1, 0, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, z, q, t, 0.03125F, 1, 0, 0, ad);
+                    this.vertex(pose, vertexConsumer, x, y, q, s, 0.03125F, 1, 0, 0, ad);
+                }
             }
         }
     }
 
     private void vertex(
-            PoseStack.Pose pose, @NotNull VertexConsumer photoConsumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int light
+            PoseStack.Pose pose, @NotNull VertexConsumer vertexConsumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int light
     ) {
-        photoConsumer.addVertex(pose, x, y, z)
+        vertexConsumer.addVertex(pose, x, y, z)
                 .setColor(-1)
                 .setUv(u, v)
                 .setOverlay(OverlayTexture.NO_OVERLAY)
