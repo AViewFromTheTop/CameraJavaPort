89ee8254ea7456f96299bfaffacea78d4423f9e6
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-09-25 14:01:40 -0400

    move items to item class

--- src/main/java/net/lunade/camera/CameraPortMain.java
+++ src/main/java/net/lunade/camera/CameraPortMain.java
@@ -1,8 +1,6 @@
 package net.lunade.camera;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.lunade.camera.item.CameraItem;
 import net.lunade.camera.networking.CameraNetworking;
 import net.lunade.camera.registry.CameraBlocks;
 import net.lunade.camera.registry.CameraEntityTypes;
@@ -10,16 +8,9 @@ import net.lunade.camera.registry.CameraItems;
 import net.lunade.camera.registry.CameraMenuTypes;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 
 public class CameraPortMain implements ModInitializer {
-    public static final CameraItem CAMERA_ITEM = new CameraItem(CameraEntityTypes.CAMERA, 0, 0, new Item.Properties());
-    public static final CameraItem DISC_CAMERA_ITEM = new CameraItem(CameraEntityTypes.DISC_CAMERA, 0, 0, new Item.Properties());
-
     public static final SoundEvent CAMERA_BREAK = SoundEvent.createVariableRangeEvent(CameraConstants.id("entity.camera.break"));
     public static final SoundEvent CAMERA_FALL = SoundEvent.createVariableRangeEvent(CameraConstants.id("entity.camera.fall"));
     public static final SoundEvent CAMERA_HIT = SoundEvent.createVariableRangeEvent(CameraConstants.id("entity.camera.hit"));
@@ -27,12 +18,6 @@ public class CameraPortMain implements ModInitializer {
     public static final SoundEvent CAMERA_PRIME = SoundEvent.createVariableRangeEvent(CameraConstants.id("entity.camera.prime"));
     public static final SoundEvent CAMERA_SNAP = SoundEvent.createVariableRangeEvent(CameraConstants.id("entity.camera.snap"));
     public static final SoundEvent CAMERA_ADJUST = SoundEvent.createVariableRangeEvent(CameraConstants.id("entity.camera.adjust"));
-
-    public static CameraItem registerCamera(CameraItem cameraItem, ResourceLocation resourceLocation) {
-        Registry.register(BuiltInRegistries.ITEM, resourceLocation, cameraItem);
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register((entries) -> entries.addAfter(Items.LODESTONE, cameraItem));
-        return cameraItem;
-    }
 
     @Override
     public void onInitialize() {
@@ -43,8 +28,6 @@ public class CameraPortMain implements ModInitializer {
         Registry.register(BuiltInRegistries.SOUND_EVENT, CAMERA_PRIME.getLocation(), CAMERA_PRIME);
         Registry.register(BuiltInRegistries.SOUND_EVENT, CAMERA_SNAP.getLocation(), CAMERA_SNAP);
         Registry.register(BuiltInRegistries.SOUND_EVENT, CAMERA_ADJUST.getLocation(), CAMERA_ADJUST);
-        registerCamera(DISC_CAMERA_ITEM, CameraConstants.id("disc_camera"));
-        registerCamera(CAMERA_ITEM, CameraConstants.id("camera"));
         CameraBlocks.register();
         CameraItems.register();
         CameraMenuTypes.register();
--- src/main/java/net/lunade/camera/registry/CameraItems.java
+++ src/main/java/net/lunade/camera/registry/CameraItems.java
@@ -3,15 +3,19 @@ package net.lunade.camera.registry;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.lunade.camera.CameraConstants;
 import net.lunade.camera.component.PictureComponent;
+import net.lunade.camera.item.CameraItem;
 import net.lunade.camera.item.PictureItem;
 import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 public class CameraItems {
+    public static final CameraItem CAMERA_ITEM = new CameraItem(CameraEntityTypes.CAMERA, 0, 0, new Item.Properties());
+    public static final CameraItem DISC_CAMERA_ITEM = new CameraItem(CameraEntityTypes.DISC_CAMERA, 0, 0, new Item.Properties());
     public static final PictureItem PICTURE = new PictureItem(new Item.Properties().stacksTo(1));
 
     public static final DataComponentType<PictureComponent> PHOTO_COMPONENT = DataComponentType.<PictureComponent>builder()
@@ -19,10 +23,19 @@ public class CameraItems {
             .networkSynchronized(PictureComponent.STREAM_CODEC)
             .build();
 
+
     public static void register() {
+        registerCameraItem(DISC_CAMERA_ITEM, CameraConstants.id("disc_camera"));
+        registerCameraItem(CAMERA_ITEM, CameraConstants.id("camera"));
+
         Registry.register(BuiltInRegistries.ITEM, CameraConstants.id("picture"), PICTURE);
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register((entries) -> entries.addAfter(Items.LODESTONE, PICTURE));
 
         Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, CameraConstants.id("picture_component"), PHOTO_COMPONENT);
+    }
+
+    public static void registerCameraItem(CameraItem cameraItem, ResourceLocation resourceLocation) {
+        Registry.register(BuiltInRegistries.ITEM, resourceLocation, cameraItem);
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register((entries) -> entries.addAfter(Items.LODESTONE, cameraItem));
     }
 }
