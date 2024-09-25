f273f7d345211d57552c64eb339cc8c031a67b47
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-09-25 03:56:18 -0400

    remove photos that havent been rendered in five seconds

--- src/main/java/net/lunade/camera/impl/client/PhotographLoader.java
+++ src/main/java/net/lunade/camera/impl/client/PhotographLoader.java
@@ -7,9 +7,11 @@ import net.frozenblock.lib.image_transfer.FileTransferPacket;
 import net.frozenblock.lib.image_transfer.client.ServerTexture;
 import net.lunade.camera.CameraConstants;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,24 +19,25 @@ import java.util.stream.Stream;
 
 @Environment(EnvType.CLIENT)
 public class PhotographLoader {
-    private static final ArrayList<ResourceLocation> LOADED_TEXTURES = new ArrayList<>();
+    private static final TextureManager TEXTURE_MANAGER = Minecraft.getInstance().getTextureManager();
     private static final ArrayList<ResourceLocation> LOADING_PICTURES = new ArrayList<>();
 
     public static ResourceLocation get(int index) {
         return LOADING_PICTURES.get(index);
     }
 
-    public static ResourceLocation get(String pictureName) {
+    public static @NotNull ResourceLocation get(String pictureName) {
         final var location = getPhotographLocation(pictureName);
-        if (!LOADED_TEXTURES.contains(location)) {
+        if (!(TEXTURE_MANAGER.getTexture(location, null) instanceof ServerTexture serverTexture)) {
             final var serverTexture = new ServerTexture(
                     "photographs",
                     pictureName + ".png",
                     CameraConstants.id("photographs/empty"),
                     () -> {}
             );
-            Minecraft.getInstance().getTextureManager().getTexture(location, serverTexture);
-            LOADED_TEXTURES.add(location);
+            TEXTURE_MANAGER.getTexture(location, serverTexture);
+        } else {
+            serverTexture.updateReferenceTime();
         }
         return location;
     }
@@ -43,14 +46,14 @@ public class PhotographLoader {
         return LOADING_PICTURES.size();
     }
 
-    public static ResourceLocation getInfinite(int index) {
-        if(LOADING_PICTURES.isEmpty()) return null;
+    public static @Nullable ResourceLocation getInfinite(int index) {
+        if (LOADING_PICTURES.isEmpty()) return null;
         int size = LOADING_PICTURES.size();
         int adjustedIndex = ((index % size) + size) % size;
         return get(adjustedIndex);
     }
 
-    private static ResourceLocation getPhotographLocation(@NotNull String name) {
+    private static @NotNull ResourceLocation getPhotographLocation(@NotNull String name) {
         return CameraConstants.id("photographs/" + name);
     }
 
@@ -58,7 +61,7 @@ public class PhotographLoader {
         final File file = FabricLoader.getInstance().getGameDir().resolve("photographs").toFile();
         final var list = file.listFiles();
         LOADING_PICTURES.clear();
-        if(list != null) {
+        if (list != null) {
             for (String name : Stream.of(list).map(File::getName).toList()) {
                 name = name.replace(".png", "");
                 LOADING_PICTURES.add(PhotographLoader.get(name));
